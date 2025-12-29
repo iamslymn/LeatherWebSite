1b0525816d56c171c1562625e0e90941b8952778
iamslymn <113949283+iamslymn@users.noreply.github.com>
2025-12-29 04:21:43 +0400

    feat: enable edge runtime for product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const runtime = 'edge';
 import React, { useState } from 'react';
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
